67b07a06c1884f9a211948c379ea6ad6f955809c
Teresa <TC807496@wcupa.edu>
2016-11-01 21:20:10 -0400

    Added numbers to tile grid and created a GameManager

---
+++ Assets/Scripts/TileNumbers.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[System.Serializable]
+public class Tiles
+{
+	public int Number;
+	public Sprite tile;
+
+}
+
+
+public class TileNumbers : MonoBehaviour {
+
+	public static TileNumbers instance;
+
+	public Tiles[] TilesNum;
+
+	void Awake()
+	{
+		instance = this;
+
+	}
+}
