84e82221f9dda274f496c0329f969fef1b116139
Teresa <TC807496@wcupa.edu>
2016-11-04 00:46:07 -0400

    Created a Game Over and Won GameObject

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,14 @@
 ﻿using UnityEngine;
 using System.Collections;
-using System.Collections.Generic; //19
+using System.Collections.Generic; 
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour {
-	//public static GameManager instance;
+
+	public GameObject WonText;
+	public GameObject GameOverText;
+	public Text GameOverScoreText;
+	public GameObject GameOverPanel;
 
 	private Tile[,] AllTiles = new Tile[4, 4];
 	private List<Tile[]> columns = new List<Tile[]> ();
@@ -30,7 +35,57 @@ public class GameManager : MonoBehaviour {
 		rows.Add(new Tile[]{AllTiles[1,0], AllTiles[1,1], AllTiles[1,2], AllTiles[1,3]});
 		rows.Add(new Tile[]{AllTiles[2,0], AllTiles[2,1], AllTiles[2,2], AllTiles[2,3]});
 		rows.Add(new Tile[]{AllTiles[3,0], AllTiles[3,1], AllTiles[3,2], AllTiles[3,3]});
+	
+		Generate ();
+		Generate ();
 	}
+
+
+	private void GameOver()
+	{
+		GameOverScoreText.text = ScoreTracker.instance.Score.ToString ();
+		GameOverPanel.SetActive (true);
+
+	}
+
+	private void Won()
+	{
+		GameOverText.SetActive (false);
+		WonText.SetActive (true);
+		GameOverScoreText.text = ScoreTracker.instance.Score.ToString ();
+		GameOverPanel.SetActive (true);
+	}
+
+	bool CanMove()
+	{
+		if (EmptyTiles.Count > 0)
+			return true;
+		else 
+		{
+			for (int i = 0; i < columns.Count; i++) {
+				for (int j = 0; j < rows.Count - 1; j++) {
+					if (AllTiles [j, i].Number == AllTiles [j + 1, i].Number)
+						return true;
+				}
+			}
+
+			for (int i = 0; i < rows.Count; i++) {
+				for (int j = 0; j < columns.Count - 1; j++) {
+					if (AllTiles [i, j].Number == AllTiles [i, j + 1].Number)
+						return true;
+				}
+			}
+		}
+		return false;
+			
+	}
+
+
+	public void NewGameButtonHandler()
+	{
+		Application.LoadLevel (Application.loadedLevel);
+	}
+
 
 	bool MakeOneMoveDownIndex(Tile[] LineOfTiles)
 	{
@@ -48,8 +103,13 @@ public class GameManager : MonoBehaviour {
 			{
 				LineOfTiles [i].Number *= 2;
 				LineOfTiles[i+1].Number = 0;
-					LineOfTiles[i].mergedThisTurn =true;
-					return true;
+				LineOfTiles[i].mergedThisTurn =true;
+				ScoreTracker.instance.Score += LineOfTiles [i].Number;
+				if (LineOfTiles [i].Number == 2048) 
+				{
+					Won ();
+				}	
+				return true;
 			}
 		}
 		return false;
@@ -73,6 +133,11 @@ public class GameManager : MonoBehaviour {
 				LineOfTiles [i].Number *= 2;
 				LineOfTiles[i-1].Number = 0;
 				LineOfTiles[i].mergedThisTurn =true;
+				ScoreTracker.instance.Score += LineOfTiles [i].Number;
+				if (LineOfTiles [i].Number == 2048) 
+				{
+					Won ();
+				}
 				return true;
 			}
 		}
@@ -96,11 +161,11 @@ public class GameManager : MonoBehaviour {
 	}
 
 	// Update is called once per frame
-	void Update () 
+	/*void Update () 
 	{
 		if (Input.GetKeyDown(KeyCode.G))
 			Generate ();
-	}
+	}*/
 
 	private void ResetMergedFlags()
 	{
@@ -109,26 +174,55 @@ public class GameManager : MonoBehaviour {
 	}
 
 
+	private void UpdateEmptyTiles()
+	{
+		EmptyTiles.Clear ();
+		foreach (Tile t in AllTiles) {
+			if (t.Number == 0) {
+				EmptyTiles.Add (t);
+			
+			}
+		}
+	}
+
 	public void Move(MoveDirection md)
 	{
 		Debug.Log (md.ToString() + " move.");
+		bool moveMade = false;
 		ResetMergedFlags ();
 		for (int i = 0; i < rows.Count; i++) 
 		{
 			switch (md) 
 			{
 			case MoveDirection.Down:
-				while (MakeOneMoveUpIndex(columns[i])) {}
+				while (MakeOneMoveUpIndex(columns[i])) {
+					moveMade = true;
+				}
 				break;
 			case MoveDirection.Left:
-				while (MakeOneMoveDownIndex(rows[i])) {}
+				while (MakeOneMoveDownIndex(rows[i])) {
+					moveMade = true;
+				}
 				break;
 			case MoveDirection.Right:
-				while (MakeOneMoveUpIndex(rows[i])) {}
+				while (MakeOneMoveUpIndex(rows[i])) {
+					moveMade = true;
+				}
 				break;
 			case MoveDirection.Up:
-				while (MakeOneMoveDownIndex(columns[i])){}
+				while (MakeOneMoveDownIndex(columns[i])){
+					moveMade = true;
+				}
 				break;
+			}
+		}
+		if (moveMade) {
+			UpdateEmptyTiles ();
+			Generate ();
+
+			if (!CanMove ()) 
+			{
+				GameOver ();
 			}
 		}
 	}
---
+++ Assets/Scripts/ScoreTracker.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class ScoreTracker : MonoBehaviour {
+
+	private int score;
+	public static ScoreTracker instance;
+	public Text ScoreText;
+	public Text HighScoreText;
+
+	public int Score
+	{
+		get
+		{
+			return score;
+		}
+		set
+		{
+			score = value;
+			ScoreText.text = score.ToString ();
+
+			if (PlayerPrefs.GetInt ("HighScore") < score) 
+			{
+				PlayerPrefs.SetInt ("HighScore", score);
+				HighScoreText.text = score.ToString ();
+			}
+		}
+	}
+
+
+	void Awake()
+	{
+		instance = this;
+
+		if (!PlayerPrefs.HasKey ("HighScore"))
+			PlayerPrefs.SetInt ("HighScore", 0);
+
+		ScoreText.text = "0";
+		HighScoreText.text = PlayerPrefs.GetInt ("HighScore").ToString();
+
+
+		
+	}
+
+
+}
